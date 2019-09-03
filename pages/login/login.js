cabd69b45d32d00fb732bec3cc97c5500e6227e1
haidong <1742482320@qq.com>
2019-09-03 13:05:31 +0800

    控制分享

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,56 +3,63 @@ const api = require('../../api/api.js')
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    username: '',
-    isloading: false
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        username: '',
+        isloading: false
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    wx.getStorage({
-      key: 'userInfo',
-      success: (res) => {
-        this.setData({
-          isloading: true
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        wx.getStorage({
+            key: 'userInfo',
+            success: (res) => {
+                this.setData({
+                    isloading: true
+                })
+                setTimeout(() => {
+                    wx.reLaunch({
+                        url: '../index/index',
+                    })
+                }, 1000)
+            }
         })
-        setTimeout(()=>{
-          wx.reLaunch({
-            url: '../index/index',
-          })
-        }, 1000)
-      }
-    })
-  },
+    },
 
-  inpFuc(e) {
-    this.setData({
-      username: e.detail.e.detail.value
-    })
-  },
+    inpFuc(e) {
+        this.setData({
+            username: e.detail.e.detail.value
+        })
+    },
 
-  login() {
-    this.setData({
-      isloading: true
-    })
-    api.login(this.data.username).then((res) => {
-      console.log('登录数据 ==> ', res)
-      wx.setStorageSync('userInfo', res)
-      wx.reLaunch({
-        url: '../index/index',
-      })
-    })
-  }
+    login() {
+        this.setData({
+            isloading: true
+        })
+        api.login(this.data.username).then((res) => {
+            console.log('登录数据 ==> ', res)
+            wx.setStorageSync('userInfo', res)
+            wx.reLaunch({
+                url: '../index/index',
+            })
+        })
+    }
 })
